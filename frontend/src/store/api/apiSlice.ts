936680dafcb3cb59a0ebdab246905f25ea7ad015
OzoneBht1 <np03cs4s210036@heraldcollege.edu.np>
2023-01-31 00:15:23 +0545

    Add and implement refresh token in frontend

--- frontend/src/store/api/apiSlice.ts
+++ frontend/src/store/api/apiSlice.ts
@@ -1,5 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { tokenState } from "../../components/types/types";
+import type {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query";
+import { authActions } from "../auth-slice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:8000/api",
@@ -8,14 +14,78 @@ const baseQuery = fetchBaseQuery({
     const tokenObj: tokenState = JSON.parse(token as string);
     console.log(tokenObj);
     if (token) {
+      // checkToken(tokenObj);
+
       headers.set("authorization", `Bearer ${tokenObj.access}`);
     }
     return headers;
   },
 });
 
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+  console.log(result);
+
+  if (result.error && result.error.status === 401) {
+    const tokens = localStorage.getItem("authTokens");
+    const tokenObj: tokenState = JSON.parse(tokens as string);
+    console.log(tokens);
+    console.log(args, api, extraOptions);
+    console.log(tokenObj);
+    const refreshResult = await baseQuery(
+      {
+        url: "token/refresh/",
+        method: "POST",
+        body: {
+          refresh: tokenObj.refresh,
+        },
+      },
+      api,
+      extraOptions
+    );
+
+    console.log(refreshResult);
+    if (refreshResult.data) {
+      const refreshResultData = refreshResult.data as { access: string };
+      console.log(refreshResult.data);
+      localStorage.setItem(
+        "authTokens",
+        JSON.stringify({
+          access: refreshResultData.access,
+          refresh: tokenObj.refresh,
+        })
+      );
+      api.dispatch(
+        authActions.setCredentials({
+          authTokens: {
+            access: refreshResultData.access,
+            refresh: tokenObj.refresh,
+          },
+        })
+      );
+
+      return baseQuery(args, api, extraOptions);
+    }
+  }
+
+  // if (result.error) {
+  //   const token = localStorage.getItem("authTokens");
+  //   const tokenObj: tokenState = JSON.parse(token as string);
+
+  //   const token = await baseQuery("token/refresh/", api, extraOptions);
+
+  //   localStorage.setItem("authTokens", JSON.stringify(newToken));
+  //   return api.baseQuery(args, api, extraOptions);
+  // }
+  return result;
+  // return result;
+};
 export const apiSlice = createApi({
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithReauth,
   reducerPath: "api",
   endpoints: () => ({}),
 });
