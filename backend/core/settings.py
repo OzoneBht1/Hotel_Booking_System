32b875535408dc7db426801e2c5091d3e184fc26
OzoneBht1 <np03cs4s210036@heraldcollege.edu.np>
2023-02-11 17:00:45 +0545

    Add default pagination class

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -145,7 +145,9 @@ AUTH_USER_MODEL = 'account_manager.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 30
 }
 
 
